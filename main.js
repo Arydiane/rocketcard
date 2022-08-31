778291cdb2b96be5afd162165797bd973c0d1243
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-08-31 11:08:47 -0300

    function to consume Github API

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+const url = "https://api.github.com/users"
+const userGitHub = "arydiane"
 
+function getUserGitHub(){
+    fetch(`${url}/${userGitHub}`)
+        .then(response => response.json())
+        .then(data => {     
+            nameUser.textContent = data.login;
+            followers.textContent = data.followers > 1 ? `${data.followers} Seguidores`: `${data.followers} Seguidor}`;
+            following.textContent = `${data.following} Seguindo`;
+            repository.textContent = data.public_repos> 1 ? `${data.public_repos} Repositórios` : `${data.public_repos} Repositório`;
+            company.textContent = data.company === null ? `----` : `${data.company}`;
+            locationUser.textContent = data.location === null ? `----` : `${data.location}`
+        })
+        .catch( error => console.error(error))
+}
 
 function generateColor() {
 
@@ -10,3 +25,5 @@ function generateColor() {
 
     btn.style.backgroundColor = `rgb(${r},${g},${b})`
 }
+
+getUserGitHub()
